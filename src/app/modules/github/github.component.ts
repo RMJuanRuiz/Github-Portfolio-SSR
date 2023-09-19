a2fac96750a3c973697cdaca56cf6d2987525c9a
Juan Ruiz <rmjuanruiz@gmail.com>
2023-09-19 17:54:00 -0500

    Add title and meta tags dinamically

--- src/app/modules/github/github.component.ts
+++ src/app/modules/github/github.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { GithubService } from './services/github.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.scss']
+  styleUrls: ['./github.component.scss'],
 })
 export class GithubComponent {
   username = '';
 
-  constructor(private githubService: GithubService){}
+  constructor(
+    private githubService: GithubService,
+    private titleService: Title,
+    private meta: Meta
+  ) {}
 
   ngOnInit(): void {
     this.username = this.githubService.getUserName();
+    this.titleService.setTitle('Github Portfolio');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s Github Portfolio`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
   }
-  
 }
